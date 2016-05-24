912ee42e8f805f0854c1180778a168d84670585e
mitmirzutun <milan1111@gmx.de>
2016-05-24 12:21:06 +0200

    Prozent hinzugefügt

---
+++ cleaned_quota.php
@@ -0,0 +1,23 @@
+<?php
+
+$cacheDir = "archiv/";
+$fileName="201604";
+
+$fileInputCSV = $fileName."_extracted.csv";
+$fileOutputCSV = $fileName."_cleaned.csv";
+$fileOutputCSVPath = $cacheDir.$fileOutputCSV;
+$fileInputCSVPath = $cacheDir.$fileInputCSV;
+
+$fh_inp = fopen($fileInputCSVPath, 'r');
+$fh_out = fopen($fileOutputCSVPath, 'w+');
+// calculate cleaned_total, and cleaned_quota 
+while($line = fgetcsv($fh_inp)) {
+    $a = (($line[6]+$line[8]+$line[9])/($line[6]+$line[8]+$line[9]+$line[11]))*100;
+    $a = number_format($a, 1,"," ,".");
+    array_push($line, $line[6]+$line[8]+$line[9], $line[6]+$line[8]+$line[9]+$line[11], $a);
+    print_r($line);
+    fputcsv($fh_out, $line);
+  }
+fclose($fh_inp);
+fclose($fh_out);
+?>
