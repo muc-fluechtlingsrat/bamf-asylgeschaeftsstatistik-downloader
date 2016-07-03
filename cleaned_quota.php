f7650672a24a04c54b9897050d9eed11bafd2a5a
Suny Kim <ynux@gmx.net>
2016-07-03 13:43:26 +0200

    run for the last 4 months

--- cleaned_quota.php
+++ cleaned_quota.php
@@ -1,21 +1,29 @@
 <?php
 
-$cacheDir = "archiv/";
-$fileName="201604";
-
-$fileInputCSV = $fileName."_extracted.csv";
-$fileOutputCSV = $fileName."_cleaned.csv";
-$fileOutputCSVPath = $cacheDir.$fileOutputCSV;
-$fileInputCSVPath = $cacheDir.$fileInputCSV;
-
-$fh_inp = fopen($fileInputCSVPath, 'r');
-$fh_out = fopen($fileOutputCSVPath, 'w+');
-// calculate cleaned_total, and cleaned_quota 
-while($line = fgetcsv($fh_inp)) {
-    $a = (($line[6]+$line[8]+$line[9])/($line[6]+$line[8]+$line[9]+$line[11]))*100;
-    $a = number_format($a, 1,"," ,".");
-    array_push($line, $line[6]+$line[8]+$line[9], $line[6]+$line[8]+$line[9]+$line[11], $a);
-    fputcsv($fh_out, $line);
-  }
-fclose($fh_inp);
+$date = new \DateTime();
+for($m = 0; $m <= 4; $m++) {
+  $fileName = $date->format('Y').$date->format('m');
+    $fileInputCSV = $fileName."_extracted.csv";
+    $fileOutputCSV = $fileName."_cleaned.csv";
+    $cacheDir = "archiv/";
+    $fileInputCSV = $fileName."_extracted.csv";
+    $fileOutputCSV = $fileName."_cleaned.csv";
+    $fileOutputCSVPath = $cacheDir.$fileOutputCSV;
+    $fileInputCSVPath = $cacheDir.$fileInputCSV;
+    print_r($fileInputCSVPath); 
+    if (file_exists($fileInputCSVPath)) {
+      $fh_inp = fopen($fileInputCSVPath, 'r');
+      $fh_out = fopen($fileOutputCSVPath, 'w+');
+      // calculate cleaned_total, and cleaned_quota 
+      while($line = fgetcsv($fh_inp)) {
+          $a = (($line[6]+$line[8]+$line[9])/($line[6]+$line[8]+$line[9]+$line[11]))*100;
+          $a = number_format($a, 1,"," ,".");
+          array_push($line, $line[6]+$line[8]+$line[9], $line[6]+$line[8]+$line[9]+$line[11], $a);
+          fputcsv($fh_out, $line);
+        }
+      fclose($fh_inp);
+      fclose($fh_out);
+    }
+  $date->sub(new \DateInterval('P1M'));
+  } 
 ?>
